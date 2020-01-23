9fcc82ce4cd7cc08c988b010ca30f63f8cb8cfe0
Bistika <48261571+Bistika@users.noreply.github.com>
2020-01-23 18:01:03 +0200

    done

--- Parser/src/com/shop/Main.java
+++ Parser/src/com/shop/Main.java
@@ -7,7 +7,6 @@ public class Main {
 
     public static void main(String[] args) {
         String string = "0832 973213 ada 074o-123 312 0721 912 123 123 321 212 0732-222111(7480)-732213 0932-213-231(0732)-1321220749 223-102a0792-010-110";
-        String phoneno = null;
         Pattern p = Pattern.compile("\\(?07\\d{2}\\)?(-|\\s)\\d{3}(-|\\s)?\\d{3}");
         Matcher m = p.matcher(string);
         while (m.find()) {
